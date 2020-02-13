8ae58e53a7a7abe9879379f966c42d4ee5a4e075
marqinhos <marcosf12400@gmail.com>
2020-02-13 13:08:48 +0100

    holaaa

---
+++ talker_py/scripts/talker.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python     
+# license removed for brevity    
+ 
+import rospy
+from std_msgs.msg import String 
+#importante el primer comentario no dejar un espacio
+def talker():     
+    pub = rospy.Publisher('chatter', String, queue_size=10)     
+    rospy.init_node('talker', anonymous=True) 
+    rate = rospy.Rate(10) # 10hz     
+    counter=0     
+    while not rospy.is_shutdown():         
+        hello_str = "hello world %d" % counter         
+        counter += 1
+        rospy.loginfo(hello_str)         
+        pub.publish(hello_str)         
+        rate.sleep()
+
+if __name__ == '__main__':     
+    try:         
+        talker()     
+        
+    except rospy.ROSInterruptException:         
+        pass 
