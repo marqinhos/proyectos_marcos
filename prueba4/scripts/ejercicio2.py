1e6be575e08454abe6a2df2ce790e118bf70e2bb
marqinhos <marcosf12400@gmail.com>
2020-02-20 11:57:59 +0100

    ejercicio2

---
+++ prueba4/scripts/ejercicio2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python     
+# license removed for brevity    
+ 
+import rospy
+from std_msgs.msg import String 
+
+#importante el primer comentario no dejar un espacio
+
+def callback(msg):     
+    rospy.loginfo("mensaje recibido: %s",msg.data) 
+
+def memory_subscriber():     
+    rospy.init_node('suscriptor_basico_marcos',anonymous=True)     
+    #el primer argumento, es el nombrel del topic al que queremos suscribirnos...     
+    #el segundo argumento es el tipo de mensaje,      
+    rospy.Subscriber('num',String,callback)     
+    rospy.spin()
+
+if __name__ == "__main__":     
+    try:         
+        memory_subscriber()     
+    
+    except rospy.ROSInterruptException:         
+        pass
